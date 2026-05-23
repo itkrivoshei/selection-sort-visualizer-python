54d4ee14c0a120f26ab9c30b1efbd84c600998d6
Nikita Krivoshei <69799139+itkrivoshei@users.noreply.github.com>
2026-05-23 15:39:21 +0200

    Add package entry point

---
+++ src/selection_sort_visualizer/__init__.py
@@ -0,0 +1,5 @@
+"""Selection sort visualization utilities."""
+
+from .sorting import SortStep, iter_selection_sort, selection_sort
+
+__all__ = ["SortStep", "iter_selection_sort", "selection_sort"]
